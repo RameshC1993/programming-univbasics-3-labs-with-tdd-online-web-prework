5dba46d75c75cb9b4d52e2d438d1009207ce734a
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-12-25 18:49:16 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,7 @@ require_relative '../calculator.rb'
 describe "./calculator.rb"
 
 def calculator 
-  var first_number = 
+  var first_number = get_variable_from_file('./calculator.rb', "first_number")
 end
 
 
